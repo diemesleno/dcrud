1985f5cb89c995f8de58a44fc10c9815dc221efd
Diemesleno Souza Carvalho <diemesleno@gmail.com>
2018-02-28 07:32:44 -0300

    changing DATABASE confs

--- bank/settings.py
+++ bank/settings.py
@@ -91,10 +91,10 @@ WSGI_APPLICATION = 'bank.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',
+        'NAME': 'dcrud',
+        'USER': 'simetric',
+        'PASSWORD': 'amdk62500',
+        'HOST': '192.168.1.11',
         'PORT': '5432',
     }
 }
