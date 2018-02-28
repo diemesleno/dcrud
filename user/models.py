5fec22538deb06f5c516a8850d185d92f785bad1
Diemesleno Souza Carvalho <diemesleno@gmail.com>
2018-02-28 06:35:24 -0300

    Creating the User model, based on AbstractUser

--- user/models.py
+++ user/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from localflavor.generic.models import IBANField
+from django.conf import settings
 
-# Create your models here.
+'''
+The user extend AbstractUser and I added 2 new fields
+'''
+class User(AbstractUser):
+    iban = IBANField()
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE)
+    
+    def save(self, *args, **kwargs):
+        '''
+        If the user create account using Google Authentication he will
+        no have creator but in this case will be superuser. If the user
+        was created by a superuser, so will be a normal user.
+
+        As we don't need username here, I put the email hash as username.
+        '''
+        if not self.creator:
+            self.is_superuser = True
+        else:
+            self.is_superuser = False
+        self.username = hash(self.email)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+    	'''
+        Just to not return a object without information
+    	'''
+        return self.last_name + ', ' + self.first_name
