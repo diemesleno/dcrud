7c856fbd728bb1dd1341cc50b0ac2e07f83943f8
Diemesleno Souza Carvalho <diemesleno@gmail.com>
2018-03-04 13:00:20 -0300

    Improving the email/username field

--- user/models.py
+++ user/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import AbstractUser
 from localflavor.generic.models import IBANField
 from django.conf import settings
 
+import uuid
+
 
 class User(AbstractUser):
     '''
@@ -10,6 +12,10 @@ class User(AbstractUser):
     '''
     iban = IBANField()
     creator = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE)
+    email = models.EmailField('E-mail', unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
     
     def save(self, *args, **kwargs):
         '''
@@ -18,14 +24,15 @@ class User(AbstractUser):
         was created by a superuser, so he will be a normal user.
 
         If the user is a superuser, the username will be the email. it not,
-        we don't need the username.
+        we don't need the username, so I put a hash on it.
         '''
         if not self.creator:
             self.is_superuser = True
             self.username = self.email
         else:
             self.is_superuser = False
-            self.username = ''
+            self.username = str(uuid.uuid4())
+            self.email = str(uuid.uuid4())
         super().save(*args, **kwargs)
 
     def __str__(self):
